c75f79aa0365a4f70ab73a08cbd81486c67d0177
niochi273 <valeraveselovsky2000@gmail.com>
2026-08-08 16:04:05 +0700

    Check if authenticated

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -9,7 +9,7 @@ export default async function RootLayout({
 }>) {
   const session = await auth.api.getSession({ headers: await headers() });
 
-  if (!session) redirect("/home");
+  if (session) redirect("/home");
 
   return <>{children}</>;
 }
